7cdd5df059f853ba7335d15987bef95157bdc001
Doheera-kosi <kupourevans99@gmail.com>
2024-03-24 23:31:05 +0000

    Added Header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,10 +4,12 @@ import SignUp from "./pages/SignUp";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sign-up" element={<SignUp />} />
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function SignIn() {
   return (
-    <div>SignIn</div>
+    <div>
+      
+    </div>
   )
 }
