f68e5524e4f9a2194bcf031c2df75cec9cb2cc2c
Doheera-kosi <kupourevans99@gmail.com>
2024-03-26 22:25:11 +0000

    Update user model with image/photo type

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -14,6 +14,11 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: true,
+  },
+
+  avatar: {
+    type: String,
+    default: 'https://t4.ftcdn.net/jpg/05/09/59/75/240_F_509597532_RKUuYsERhODmkxkZd82pSHnFtDAtgbzJ.jpg'
   }
 }, {timestamps: true})
 
