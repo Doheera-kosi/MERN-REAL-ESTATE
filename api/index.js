09d136a06ba562e50094afa8c158698e5c7d2130
Doheera-kosi <kupourevans99@gmail.com>
2024-03-26 14:33:32 +0000

    Add error handler description

--- api/index.js
+++ api/index.js
@@ -23,6 +23,7 @@ app.listen(3000, () => {
 app.use('/api/user', userRouter)
 app.use('/api/auth', authRouter)
 
+// ErrorHandler MiddleWare
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || 'Internal Server Error!';
