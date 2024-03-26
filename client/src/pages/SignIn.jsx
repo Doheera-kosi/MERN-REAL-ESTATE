2938af31792bde758b02312542167ce65be83621
Doheera-kosi <kupourevans99@gmail.com>
2024-03-26 22:33:09 +0000

    Updated SignIn page with OAuth Component

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 import { signInFailure, signInStart, signInSuccess } from "../redux/user/userSlice";
+import OAuth from "../components/OAuth";
 
 
 export default function SignIn() {
@@ -69,6 +70,7 @@ export default function SignIn() {
         >
           {loading ? "Loading..." : "Sign In"}
         </button>
+        <OAuth />
       </form>
 
       <div className="flex gap-2 mt-5">
