d7ce2d340a517e67bd9a123a2c0c5661820d31dc
Doheera-kosi <kupourevans99@gmail.com>
2024-03-29 14:35:38 +0000

    Add methods in the UI for use signout

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -11,6 +11,7 @@ import {
   deleteUserFailure,
   deleteUserStart,
   deleteUserSuccess,
+  signOutUserStart,
   updateUserFailure,
   updateUserStart,
   updateuserSuccess,
@@ -119,6 +120,25 @@ export default function Profile() {
     }
   };
 
+
+  const handleSignOut = async () => {
+    try {
+      dispatch(signOutUserStart());
+      const res = await fetch('/api/auth/signout');
+      const data = await res.json();
+      // Handle success or error response as needed
+      if(data.success === false) {
+        dispatch(deleteUserFailure(data.message))
+        return;
+      }
+      dispatch(deleteUserSuccess(data))
+    } catch (error) {
+      // Handle fetch error
+      dispatch(deleteUserFailure(error.message))
+    }
+  };
+  
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -194,7 +214,7 @@ export default function Profile() {
         >
           Delete Account
         </span>
-        <span className="text-red-700 cursor-pointer">Sign Out</span>
+        <span onClick={handleSignOut} className="text-red-700 cursor-pointer">Sign Out</span>
       </div>
 
       <p className="text-red-700 mt-5">{error ? error : ""}</p>
