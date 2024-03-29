b0d53f9cb506e3bde3430e81d15a28d294835102
Doheera-kosi <kupourevans99@gmail.com>
2024-03-29 15:34:59 +0000

    Add API route for listings

--- api/index.js
+++ api/index.js
@@ -3,7 +3,9 @@ import express from 'express';
 import mongoose from 'mongoose';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
+
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_URL).then(() => {
@@ -25,6 +27,9 @@ app.listen(3000, () => {
 
 app.use('/api/user', userRouter)
 app.use('/api/auth', authRouter)
+
+// API ROUTES FOR LISTINGS
+app.use('/api/listing', listingRouter)
 
 // ErrorHandler MiddleWare
 app.use((err, req, res, next) => {
