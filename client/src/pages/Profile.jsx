396a4ffdce63d4fe2e04d2245651ac7d5ba36481
Doheera-kosi <kupourevans99@gmail.com>
2024-03-29 19:26:39 +0000

    Add link to Create Lisint page from Profile page

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -16,6 +16,7 @@ import {
   updateUserStart,
   updateuserSuccess,
 } from "../redux/user/userSlice";
+import { Link } from 'react-router-dom';
 
 export default function Profile() {
   const fileRef = useRef(null);
@@ -203,6 +204,10 @@ export default function Profile() {
         >
           {loading ? "Loading..." : "Update"}
         </button>
+
+        <Link className="bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95" to={"/create-listing"}>
+          Create Listing
+        </Link>
       </form>
 
       <div className="flex justify-between mt-5">
