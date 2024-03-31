84cd5f6706c6140964ef4b8adb26db0311fa2728
Doheera-kosi <kupourevans99@gmail.com>
2024-03-31 14:01:22 +0000

    Add user api route for listings

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { deleteteUser, testing, updateUser } from '../controllers/user.controller.js';
+import { deleteteUser, testing, updateUser, getUserListing } from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
@@ -8,5 +8,6 @@ router.get('/testing', testing)
 
 router.post('/update/:id', verifyToken, updateUser )
 router.delete('/delete/:id', verifyToken, deleteteUser )
+router.get('/listings/:id', verifyToken, getUserListing )
 
 export default router;
